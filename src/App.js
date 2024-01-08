76a8bc719db894c8248cbf8e225b71c753e8d3e6
tahmid808 <tahmid808@gmail.com>
2024-01-08 21:30:42 +0000

    adding css in to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Brand, Article, CTA } from './components'
-
+import { Blog,Features,Footer,Header,Possibility,WhatGPT3 } from './containers'
+import { Brand, Navbar, CTA } from './components'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
